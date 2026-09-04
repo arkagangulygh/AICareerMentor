d89e37b0e658da77c44ef827d65d7d7486ce9651
AmbitiousCoderaaaa <arkaganguly321@gmail.com>
2026-09-04 13:52:51 +0530

    Add cors support

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy import text
 
 from app.database import engine,Base
@@ -22,7 +23,15 @@ app = FastAPI(
     description="AI-powered career guidance platform",
     version="1.0.0"
 )
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(auth_router)
 app.include_router(resume_router)
 app.include_router(jobs_router)
